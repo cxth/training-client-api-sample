8e4cc265d32684644b9311c2589a6d3173611ae9
cx1x <christian.realubit@gmail.com>
2019-08-06 13:38:52 +0800

    add implicit grant method

--- auth_redirection.php
+++ auth_redirection.php
@@ -6,7 +6,8 @@ include_once('config.php');
 $query = http_build_query(array(
     'client_id' => '1',
     'redirect_uri' => REDIRECT_URI,
-    'response_type' => 'code',
+    'response_type' => 'code', // access code grant
+    //'response_type' => 'token', // implicit grant, access token returned
     'scope' => '',
 ));
  
